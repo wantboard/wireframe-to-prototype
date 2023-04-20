0480e6f09454f7ede377fa7ea0abf1a39cc827d3
Wantboard <info@wantboard.com>
2023-04-20 16:28:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 
-// You can add any Javascript code here if needed
+document.addEventListener('DOMContentLoaded', function(event) {
+	var dropdown1 = document.getElementById('dropdown1');
+	var dropdown2 = document.getElementById('dropdown2');
+	var message = document.getElementById('message');
+	var form = document.querySelector('form');
+
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		
+		// Validation logic goes here
+		
+		alert('The form was submitted!');
+	});
+});
