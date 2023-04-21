30ac1c470e1f1503249222a7ded935f616ce2253
Wantboard <info@wantboard.com>
2023-04-21 14:51:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 
-// Get video elements
-const remoteVideo = document.getElementById('remote-video');
-const localVideo = document.getElementById('local-video');
+const jokeContainer = document.querySelector('.joke-container');
+const jokeText = document.querySelector('.joke');
+const generateBtn = document.querySelector('.generate-btn');
 
-// Get control buttons
-const muteButton = document.getElementById('mute-button');
-const endCallButton = document.getElementById('end-call-button');
-const switchCameraButton = document.getElementById('switch-camera-button');
-
-// Add event listeners to control buttons
-muteButton.addEventListener('click', toggleMute);
-endCallButton.addEventListener('click', endCall);
-switchCameraButton.addEventListener('click', switchCamera);
-
-// Toggle mute on local video
-function toggleMute() {
-  localVideo.muted = !localVideo.muted;
-  muteButton.textContent = localVideo.muted ? 'Unmute' : 'Mute';
-}
-
-// End the call and stop both videos
-function endCall() {
-  remoteVideo.srcObject = null;
-  localVideo.srcObject = null;
-}
-
-// Switch between front and back camera on
+generateBtn.addEventListener('click', () => {
+  fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(response => response.json())
+    .then(data => {
+      jokeText.textContent = `${data.setup} ${data.punchline}`;
+      jokeContainer.style.display = 'block';
+    })
+    .catch(error => console.error(error));
+});
