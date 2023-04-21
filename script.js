b2cbb816419df74e994a633db3ffa23cb6f3671d
Wantboard <info@wantboard.com>
2023-04-21 00:07:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 
-// No Javascript required for this example
+function validateForm() {
+	var name = document.forms["myForm"]["name"].value;
+	var email = document.forms["myForm"]["email"].value;
+	var foods = document.forms["myForm"]["foods"].value;
+
+	if (name == "") {
+		alert("Name must be filled out");
+		return false;
+	}
+
+	if (email == "") {
+		alert("Email must be filled out");
+		return false;
+	}
+
+	if (!validateEmail(email)) {
+		alert("Invalid email format");
+		return false;
+	}
+
+	if (foods == "") {
+		alert("Please select your favorite food");
+		return false;
+	}
+
+	return true;
+}
+
+function validateEmail(email) {
+	var re = /\S+@\S+\.\S+/;
+	return re.test(email);
+}
