608be57e0bb698150792526df96dccd09607d820
Wantboard <info@wantboard.com>
2023-04-21 14:44:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 
-// No JavaScript required for this page
+// Get video elements
+const remoteVideo = document.getElementById('remote-video');
+const localVideo = document.getElementById('local-video');
+
+// Get control buttons
+const muteButton = document.getElementById('mute-button');
+const endCallButton = document.getElementById('end-call-button');
+const switchCameraButton = document.getElementById('switch-camera-button');
+
+// Add event listeners to control buttons
+muteButton.addEventListener('click', toggleMute);
+endCallButton.addEventListener('click', endCall);
+switchCameraButton.addEventListener('click', switchCamera);
+
+// Toggle mute on local video
+function toggleMute() {
+  localVideo.muted = !localVideo.muted;
+  muteButton.textContent = localVideo.muted ? 'Unmute' : 'Mute';
+}
+
+// End the call and stop both videos
+function endCall() {
+  remoteVideo.srcObject = null;
+  localVideo.srcObject = null;
+}
+
+// Switch between front and back camera on
