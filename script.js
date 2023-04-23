b094f9baca88faa847220eea3c1d952c795f86c8
Wantboard <info@wantboard.com>
2023-04-23 18:13:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 
-null
+const jokeBtn = document.getElementById('generate-btn');
+const jokeText = document.getElementById('joke');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+function generateJoke() {
+  // Code to generate a random joke goes here
+  jokeText.textContent = 'Why did the chicken cross the road? To get to the other side!';
+}
+
+const jokeForm = document.getElementById('joke-form');
+
+jokeForm.addEventListener('submit', submitJoke);
+
+function submitJoke(event) {
+  event.preventDefault();
+  // Code to submit joke form data goes here
+  alert('Joke submitted!');
+}
