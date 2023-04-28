ed99ba30788432190acc59c677ebd75a1bb7c095
Wantboard <info@wantboard.com>
2023-04-27 22:43:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
+javascript
+document.getElementById('generate-joke').addEventListener('click', function() {
+    document.getElementById('joke-text').innerText = 'This is a randomly generated joke!';
+});
 
-const jokeContainer = document.getElementById('joke-container');
-const jokeText = document.getElementById('joke');
-const generateBtn = document.getElementById('generate-btn');
-const jokeForm = document.getElementById('joke-form');
-
-generateBtn.addEventListener('click', generateJoke);
-jokeForm.addEventListener('submit', submitJoke);
-
-function generateJoke() {
-  // Replace with code to generate a joke
-  const joke = 'Why did the tomato turn red? Because it saw the salad dressing!';
-  jokeText.textContent = joke;
-}
-
-function submitJoke(event) {
-  event.preventDefault();
-  // Replace with code to submit joke form
-  alert('Joke submitted!');
-  jokeForm.reset();
-}
+document.getElementById('submit-joke-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for submitting your joke!');
+});
